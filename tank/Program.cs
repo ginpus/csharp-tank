9f32619ac2303794c57639b3a759a501cf807243
ginpus <gintautas.pusinskas@gmail.com>
2021-07-08 20:39:38 +0300

    Initial commit

---
+++ tank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
